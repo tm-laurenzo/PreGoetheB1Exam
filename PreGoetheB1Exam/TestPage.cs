af0e57c5fba49561c841c964e42b9e41734c05a3
lawre <t.m.laurenzo@gmail.com>
2026-07-18 15:43:06 +0100

    Tested using C# for page definition instead of XAML

---
+++ PreGoetheB1Exam/TestPage.cs
@@ -0,0 +1,46 @@
+﻿namespace PreGoetheB1Exam
+{
+    public partial class TestPage : ContentPage
+    {
+        int count = 0;
+
+        // Named Label - declared as a member of the class
+        Label counterLabel;
+
+        public TestPage()
+        {
+            var myScrollView = new ScrollView();
+
+            var myStackLayout = new VerticalStackLayout();
+            myScrollView.Content = myStackLayout;
+
+            counterLabel = new Label
+            {
+                Text = "Current count: 0",
+                FontSize = 18,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center
+            };
+            myStackLayout.Children.Add(counterLabel);
+
+            var myButton = new Button
+            {
+                Text = "Click me",
+                HorizontalOptions = LayoutOptions.Center
+            };
+            myStackLayout.Children.Add(myButton);
+
+            myButton.Clicked += OnCounterClicked;
+
+            this.Content = myScrollView;
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            counterLabel.Text = $"Current count: {count}";
+
+            SemanticScreenReader.Announce(counterLabel.Text);
+        }
+    }
+}
