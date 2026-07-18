3235a4c94bcdda5930d6eabca12eac529da43c4e
lawre <t.m.laurenzo@gmail.com>
2026-07-18 14:31:37 +0100

    Add project files.

---
+++ PreGoetheB1Exam/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PreGoetheB1Exam
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PreGoetheB1Exam/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PreGoetheB1Exam
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
